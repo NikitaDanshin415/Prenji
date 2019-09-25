9d430fd995f11209b92adb701be1cc187ea8330d
Nikita Danshin <danshinNP@yandex.ru>
2019-09-25 19:52:28 +0500

    Add files via upload
    Начал верстку FMC

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+    $('.carusel__inner').slick({
+        arrows:false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+    $('.countries .hamburger').on('click',function(){
+        $(this).toggleClass('hamburger_active');
+        $('.countries__items').toggleClass('countries__items_active');
+
+        $('.promo .hamburger').removeClass('hamburger_active');
+        $('.header__menu').removeClass('header__menu_active');
+    });
+
+    $('.promo .hamburger').on('click',function(){
+        $(this).toggleClass('hamburger_active');
+        $('.header__menu').toggleClass('header__menu_active');
+
+        $('.countries .hamburger').removeClass('hamburger_active');
+        $('.countries__items').removeClass('countries__items_active');
+    });
+    
+});
+
