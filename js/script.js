68cf4646a9be05f902ec16b1d95e0b02bf3923fa
Nikita Danshin <danshinNP@yandex.ru>
2019-10-13 18:03:16 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,25 +1,18 @@
 $( document ).ready(function() {
-    $('.carusel__inner').slick({
-        arrows:false,
-        //autoplay: true,
-        //autoplaySpeed: 2000,
-    });
-
     $('.countries .hamburger').on('click',function(){
         $(this).toggleClass('hamburger_active');
         $('.countries__items').toggleClass('countries__items_active');
 
         $('.promo .hamburger').removeClass('hamburger_active');
-        $('.promo__menu').removeClass('promo__menu_active');
+        $('.promo__menu-items').removeClass('promo__menu-items_active');
     });
 
     $('.promo .hamburger').on('click',function(){
         $(this).toggleClass('hamburger_active');
-        $('.promo__menu').toggleClass('promo__menu_active');
+        $('.promo__menu-items').toggleClass('promo__menu-items_active');
 
         $('.countries .hamburger').removeClass('hamburger_active');
         $('.countries__items').removeClass('countries__items_active');
     });
-    
 });
 
